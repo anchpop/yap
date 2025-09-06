0aed7c198d8a8c93cc27c633bd7a740575be3f6a
Andre Popovitch <andre@popovit.ch>
2025-09-05 20:09:37 -0500

    Add Discord link to About

--- yap-frontend/src/components/about.tsx
+++ yap-frontend/src/components/about.tsx
@@ -2,7 +2,8 @@ export function About() {
   return (
     <div className="text-center text-xs text-muted-foreground mt-4">
       yap is created by <a href="https://twitter.com/chadnauseam" className="underline">Andre Popovitch</a>.{' '}
-      <a href="https://github.com/anchpop/yap" className="underline">GitHub</a>
+      <a href="https://github.com/anchpop/yap" className="underline">GitHub</a>{' | '}
+      <a href="https://discord.gg/mpgqfsH" className="underline">Discord</a>
     </div>
   )
 }
