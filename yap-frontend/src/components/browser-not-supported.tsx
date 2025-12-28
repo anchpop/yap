d8608d882c952dc960d968c0eb1dc2ba6edf89ca
Andre Popovitch <andre@popovit.ch>
2025-12-28 12:45:34 -0600

    Update unsupported message

--- yap-frontend/src/components/browser-not-supported.tsx
+++ yap-frontend/src/components/browser-not-supported.tsx
@@ -40,8 +40,8 @@ export function BrowserNotSupported() {
                       Apple Users: Update Required
                     </p>
                     <p className="text-sm">
-                      On Safari, Yap.Town requires iOS 26 or macOS 26. Please
-                      view your device settings to update.
+                      For iPhone/iPad users, Yap.Town requires iOS 26 or macOS
+                      26. Please view your device settings to update.
                     </p>
                     <p className="text-sm ">
                       If you're on a desktop platform, you can also use Chrome
