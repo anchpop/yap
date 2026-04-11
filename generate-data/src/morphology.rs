948887c43a5d323811b65cb13581ff0ded868cec
Andre Popovitch <andre@popovit.ch>
2026-04-11 02:37:30 -0700

    clippy

--- generate-data/src/morphology.rs
+++ generate-data/src/morphology.rs
@@ -52,8 +52,8 @@ fn edit_distance(a: &str, b: &str) -> usize {
     let m = a.len();
     let n = b.len();
     let mut dp = vec![vec![0usize; n + 1]; m + 1];
-    for i in 0..=m {
-        dp[i][0] = i;
+    for (i, row) in dp.iter_mut().enumerate().take(m + 1) {
+        row[0] = i;
     }
     for j in 0..=n {
         dp[0][j] = j;
